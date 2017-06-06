ee25d3aedf7f0d732ebb044b3b6c084c3a59ac38
Mike Harms <harmsm@gmail.com>
2017-06-06 06:57:40 -0700

    can now set experiment parameters

--- pytc_gui/widgets/experiment_box/experiment_dialog.py
+++ pytc_gui/widgets/experiment_box/experiment_dialog.py
@@ -7,7 +7,7 @@ __date__ = "2017-06-01"
 from PyQt5 import QtWidgets as QW
 from PyQt5 import QtGui as QG
 
-import sys
+import sys, copy
 
 class FitParamWrapper:
     """
@@ -184,19 +184,112 @@ class FitParamWrapper:
     def alias_widget(self):
         return self._alias 
 
-class ExperimentParamWrapper:
+
+class ExperimentParamWrapper(QW.QWidget):
     """
     """
 
-    def __init__(self,parent,fit,experiment):
+    def __init__(self,parent,fit,experiment,param_name,start_value,value_type,
+                 allowable_values,cutoff=100000):
         """
         """
+
+        super().__init__()
 
         self._parent = parent
         self._fit = fit
+        self._experiment = experiment
+        self._param_name = param_name
+        self._start_value = start_value
+        self._value_type = value_type
+        self._allowable_values = allowable_values
+        self._cutoff = cutoff
 
+        self.layout()
+
+    def layout(self):
+
+        self._main_layout = QW.QHBoxLayout(self)
+
+        self._label = QW.QLabel(self._param_name)
+        self._main_layout.addWidget(self._label)       
+
+        if self._value_type == "multi":
+
+            self._select_widget = QW.QComboBox(self)
+
+            try:
+                len(self._allowable_values)
+            except TypeError:
+                err = "allowable types must be a list-like object for a multi value.\n"
+                raise ValueError(err) 
+
+            self._allowable_values = copy.copy(self._allowable_values)
+            self._allowable_values.sort()
+            self._select_widget.addItems(self._allowable_values)
+
+            current_index = self._select_widget.findText(self._start_value)
+            if current_index == -1:
+                err = "current value is not in the allowable values\n"
+                raise ValueError(err)
+
+            self._select_widget.setCurrentIndex(current_index)
+            self._select_widget.currentIndexChanged.connect(self._update_multi_param)
+
+        elif self._value_type == bool:
+
+            self._select_widget = QW.QCheckBox()
+            self._select_widget.setChecked(self._start_value)
+            self._select_widget.stateChanged.connect(self._start_value)
+
+        else:
+            self._select_widget = QW.QLineEdit(self)
+
+            if self._value_type == float:
+                if self._start_value < 1/self._cutoff or self._start_value > self._cutoff:
+                    val_str = "{:.8e}".format(self._start_value)
+                else:
+                    val_str = "{:.8f}".format(self._start_value)
+            else:
+                val_str = "{}".format(self._start_value)
+
+            self._select_widget.setText(val_str)
+
+            self._select_widget.textChanged.connect(self._update_other_param)
+
+        self._main_layout.addWidget(self._select_widget)
+           
+    def _update_multi_param(self,value):
+
+        new_value = self._select_widget.currentText()
+
+        setattr(self._experiment, self._param_name, new_value)
+        if self._fit.continuous_update:
+            self._fit.emit_changed()
+
+    def _update_bool_param(self):
+              
+        setattr(self._experiment,self._param_name, self._select_wiget.checkState())
+        if self._fit.continuous_update:
+            self._fit.emit_changed() 
+
+    def _update_other_param(self):
+
+        current = self._select_widget.text()
+        try:
+            new_value = self._value_type(current)
+            color = "#FFFFFF"
+        except ValueError:
+            new_value = None
+            color = "#FFB6C1"
         
-
+        self._select_widget.setStyleSheet("QLineEdit {{ background-color: {} }}".format(color))
+        if new_value is not None:
+        
+            setattr(self._experiment,self._param_name, new_value)
+            if self._fit.continuous_update:
+                self._fit.emit_changed() 
+ 
 class ExperimentOptionsDialog(QW.QDialog):
     """
     """
@@ -226,31 +319,31 @@ class ExperimentOptionsDialog(QW.QDialog):
         self._main_layout = QW.QVBoxLayout(self)
        
         # ------------ Fit parameters --------------- 
-        self.horizontalGroupBox = QW.QGroupBox() 
+        self._fit_param_box = QW.QGroupBox() 
 
-        self._layout = QW.QGridLayout()
+        self._fit_param_layout = QW.QGridLayout()
  
-        self._layout.addWidget(QW.QLabel("param"), 0,0) 
-        self._layout.addWidget(QW.QLabel("guess"), 0,1) 
-        self._layout.addWidget(QW.QLabel("global"),0,2) 
-        self._layout.addWidget(QW.QLabel("fixed"), 0,3) 
-        self._layout.addWidget(QW.QLabel("upper"), 0,4) 
-        self._layout.addWidget(QW.QLabel("lower"), 0,5) 
+        self._fit_param_layout.addWidget(QW.QLabel("param"), 0,0) 
+        self._fit_param_layout.addWidget(QW.QLabel("guess"), 0,1) 
+        self._fit_param_layout.addWidget(QW.QLabel("global"),0,2) 
+        self._fit_param_layout.addWidget(QW.QLabel("fixed"), 0,3) 
+        self._fit_param_layout.addWidget(QW.QLabel("upper"), 0,4) 
+        self._fit_param_layout.addWidget(QW.QLabel("lower"), 0,5) 
 
         self._param_widgets = []           
         for i, k in enumerate(model_params):
 
             self._param_widgets.append(FitParamWrapper(self,self._fit,self._model_info[k]))
  
-            self._layout.addWidget(QW.QLabel(k),                        i+1,0)
-            self._layout.addWidget(self._param_widgets[-1].guess_widget,i+1,1)
-            self._layout.addWidget(self._param_widgets[-1].alias_widget,i+1,2)
-            self._layout.addWidget(self._param_widgets[-1].fixed_widget,i+1,3)
-            self._layout.addWidget(self._param_widgets[-1].lower_widget,i+1,4)
-            self._layout.addWidget(self._param_widgets[-1].upper_widget,i+1,5)
+            self._fit_param_layout.addWidget(QW.QLabel(k),                        i+1,0)
+            self._fit_param_layout.addWidget(self._param_widgets[-1].guess_widget,i+1,1)
+            self._fit_param_layout.addWidget(self._param_widgets[-1].alias_widget,i+1,2)
+            self._fit_param_layout.addWidget(self._param_widgets[-1].fixed_widget,i+1,3)
+            self._fit_param_layout.addWidget(self._param_widgets[-1].lower_widget,i+1,4)
+            self._fit_param_layout.addWidget(self._param_widgets[-1].upper_widget,i+1,5)
 
-        self.horizontalGroupBox.setLayout(self._layout)
-        self._main_layout.addWidget(self.horizontalGroupBox)
+        self._fit_param_box.setLayout(self._fit_param_layout)
+        self._main_layout.addWidget(self._fit_param_box)
 
         # ---------- Horizontal line -----------------
         hline = QW.QFrame()
@@ -263,107 +356,31 @@ class ExperimentOptionsDialog(QW.QDialog):
         expt_keys.sort()
  
         self._expt_widgets = []       
-        for k in expt_keys:
+        for param_name in expt_keys:
 
-            start_value    = self._expt_info[k][0]
-            value_type     = self._expt_info[k][1]
-            default_value  = self._expt_info[k][2] 
-             
-            #if value_type == "multi":
-  
+            start_value      = self._expt_info[param_name][0]
+            value_type       = self._expt_info[param_name][1]
+            allowable_values = self._expt_info[param_name][2] 
 
-            print(k,self._expt_info[k])
-
-"""
-class ExpOptions(QDialog):
+            self._expt_widgets.append(ExperimentParamWrapper(self,
+                                                             self._fit,
+                                                             self._experiment,
+                                                             param_name,
+                                                             start_value,
+                                                             value_type,
+                                                             allowable_values))
  
-    def __init__(self, parent,experiment):
-        super().__init__()
-      
-        self._parent = parent  
-        self._experiment = experiment
+        self._experiment_param_box = QW.QGroupBox() 
+        self._experiment_param_layout = QW.QGridLayout()
 
-        self.layout()
+        counter = 0
+        num_rows = int(round(len(self._expt_widgets)/2))
+        for i in range(num_rows):
+            for j in range(2):
+                self._experiment_param_layout.addWidget(self._expt_widgets[counter],i,j)
+                counter += 1
 
-    def layout(self):
-        main_layout = QVBoxLayout(self)
-        self._form_layout = QFormLayout()
-        self._get_info()
+        self._experiment_param_box.setLayout(self._experiment_param_layout)
+        self._main_layout.addWidget(self._experiment_param_box)
 
-        update_button = QPushButton("Update", self)
-        update_button.clicked.connect(self._update_val)
 
-        main_layout.addLayout(self._form_layout)
-        main_layout.addWidget(update_button)
-
-        self.setWindowTitle('Experiment Options')
-
-    def _get_info(self):
-        self._option_widgets = {}
-
-        classes = inspect.getmembers(self._experiment, inspect.isclass)
-        properties = inspect.getmembers(classes[0][1], lambda o: isinstance(o, property))
-
-        setters = [p[0] for p in properties if p[1].fset != None]
-
-        for i, s in enumerate(setters):
-
-            # Skip heats (which has setter we need to ignore)
-            if s in ["heats","heats_stdev"]:
-                continue
-
-            if s == "units":
-                units_names = list(self._experiment.AVAIL_UNITS)
-                units_names.sort()
-
-                starting_index = 0
-                self._option_widgets[s] = QComboBox(self)
-                for j, k in enumerate(units_names):
-                    self._option_widgets[s].addItem(k)
-                    if k == self._experiment.units:
-                        starting_index = j
-                self._option_widgets[s].setCurrentIndex(starting_index)        
-
-                name = str(s).replace("_", " ") + ": "
-                self._experiment.units = str(self._option_widgets[s].currentText())
-                self._option_widgets[s].activated[str].connect(self._units_select)
-                self._form_layout.addRow(QLabel(name), self._option_widgets[s])
-
-            else:
-                self._option_widgets[s] = QLineEdit(self)
-                self._option_widgets[s].setText(str(getattr(self._experiment,s)))
-
-                name = str(s).replace("_", " ") + ": "
-                self._form_layout.addRow(QLabel(name.title()), self._option_widgets[s])
-
-    def _update_val(self):
-
-        for k, v in self._option_widgets.items():
-   
-            try: 
-                if type(v) == QComboBox:
-                    val = str(v.currentText())
-                else:
-                    val = ast.literal_eval(str(v.text()))
-
-                setattr(self._experiment, k, val)
-
-            except ValueError:
-
-                if type(v) == QComboBox:
-                    val = str(v.currentText())
-                else:
-                    val = str(v.text())
-                
-                error_message = QMessageBox.warning(self,
-                                                    "warning",
-                                                    "Value {} is invalid".format(val),
-                                                    QMessageBox.Ok) 
-                return
-
-        self._update.update()
-        self.hide()
-               
-    def _units_select(self,units):
-        self._experiment.units = units
-"""
