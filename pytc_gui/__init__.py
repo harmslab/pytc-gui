abb9f95ccbe54e5352874803a8606dcf87f2c590
Mike Harms <harmsm@gmail.com>
2017-06-11 14:46:25 -0700

    fixing setup and __init__ for distribution

--- pytc_gui/__init__.py
+++ pytc_gui/__init__.py
@@ -1,2 +1,4 @@
 __author__ = "Hiranmayi Duvvuri"
 __date__ = "2017-01-08"
+
+__all__  = ["dialogs","fit_container","main_window","widgets"]
--- setup.py
+++ setup.py
@@ -6,18 +6,22 @@ if sys.version_info[0] < 3:
 # Try using setuptools first, if it's installed
 from setuptools import setup, find_packages
 
-packages = ["pytc_gui"]
+packages = ["pytc_gui",
+            "pytc_gui/dialogs",
+            "pytc_gui/widgets",
+            "pytc_gui/widgets/experiment_box",
+            "pytc_gui/widgets/experiment_box/experiment_dialog"]
 
 # Need to add all dependencies to setup as we go!
 setup(name='pytc-gui',
       packages=packages,
-      version='1.2.0',
+      version='1.2.1',
       description="PyQt5 GUI for pytc API",
       long_description=open("README.rst").read(),
       author='Hiranmayi Duvvuri',
       author_email='hiranmayid8@gmail.com',
       url='https://github.com/harmslab/pytc-gui',
-      download_url='https://github.com/harmslab/pytc-gui/tarball/1.2.0',
+      download_url='https://github.com/harmslab/pytc-gui/tarball/1.2.1',
       zip_safe=False,
       install_requires=["pytc-fitter>=1.1.4","seaborn","pyqt5"],
       package_data={"pytc_gui":["*.png","widgets/experiment_box/icons/*.png"]},
